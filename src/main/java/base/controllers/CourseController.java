3cf67cc837d0d10563f565a237beeb5595c87e7a
kknai <kokonai1827@gmail.com>
2023-12-14 07:01:05 +0630

    fist committing for testing not course crud finished yet

---
+++ src/main/java/base/controllers/CourseController.java
@@ -0,0 +1,31 @@
+package base.controllers;
+
+import base.daos.CourseDao;
+import base.models.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CourseController {
+
+    @Autowired
+    CourseDao courseDao ;
+
+    @GetMapping("/courseReg")
+    public String getCourseRegForm(Model model){
+        Course course = new Course();
+        course.setId(courseDao.getLatestCourseId());
+        model.addAttribute("course",course);
+        return "course/course_reg";
+    }
+    @PostMapping("/courseReg")
+    public String courseRegistration(@ModelAttribute ("course") Course course ){
+        int result = courseDao.createCourse(course);
+        return "redirect:/courseView";
+    }
+
+}
--- src/main/java/base/models/Course.java
+++ src/main/java/base/models/Course.java
@@ -1,4 +1,62 @@
 package base.models;
 
+import jakarta.persistence.*;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
 public class Course {
+
+    @Id
+    private String id ;
+
+    @NotBlank
+    private String name;
+
+    @ManyToMany(fetch= FetchType.LAZY,
+            cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+                    CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name="course_student",
+            joinColumns=@JoinColumn(name="course_id"),
+            inverseJoinColumns=@JoinColumn(name="student_id")
+    )
+    private List<Student> students;
+
+    public Course() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
